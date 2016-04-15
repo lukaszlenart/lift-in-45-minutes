e0db60fac0aa4b7870b215caa5d834197d9bc4ca
Lukasz Lenart <lukasz.lenart@gmail.com>
2016-04-15 23:16:58 +0200

    Drops date binding

--- src/main/scala/com/gruuf/snippet/Home.scala
+++ src/main/scala/com/gruuf/snippet/Home.scala
@@ -1,21 +1,16 @@
 package com.gruuf.snippet
 
-import com.gruuf.model.{Bike, Garage}
-import net.liftweb.http.{SHtml, Templates}
-import net.liftweb.sitemap.Loc.TemplateBox
 import net.liftweb.sitemap.Menu
-import net.liftweb.util.{ClearClearable, ClearNodes}
-import net.liftweb.util.Helpers._
-import org.joda.time.DateTime
+import net.liftweb.util.PassThru
 
 object Home {
-  val menu = Menu.i("menuHome") / "home"
+  val menu = Menu.i("Home") / "home"
 }
 
 class Home {
 
   def render = {
-    ".date" #> DateTime.now.toString()
+    PassThru
   }
 
 }
