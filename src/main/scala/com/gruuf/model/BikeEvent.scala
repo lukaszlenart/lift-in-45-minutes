231ad155bbca9a6b6370f3281d1d3a881f17adfc
Lukasz Lenart <lukasz.lenart@gmail.com>
2016-04-16 08:50:08 +0200

    Fixes typo

--- src/main/scala/com/gruuf/model/BikeEvent.scala
+++ src/main/scala/com/gruuf/model/BikeEvent.scala
@@ -2,4 +2,4 @@ package com.gruuf.model
 
 import org.joda.time.DateTime
 
-case class BikeEvent(id: String, bikeId: String, name: String, eventDate: DateTime, deescription: String, important: Boolean = false)
+case class BikeEvent(id: String, bikeId: String, name: String, eventDate: DateTime, description: String, important: Boolean = false)
