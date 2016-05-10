4440ce2607257422a236b6b5e86e8ed48d80cc5d
Lukasz Lenart <lukasz.lenart@gmail.com>
2016-05-10 10:52:34 +0200

    Prepares template snippet

---
+++ src/main/scala/com/gruuf/snippet/RegisterBike.scala
@@ -0,0 +1,13 @@
+package com.gruuf.snippet
+
+import net.liftweb.http._
+  import js.JsCmds
+import net.liftweb.sitemap._
+  import Menu._
+  import Loc._
+import net.liftweb.util._
+  import Helpers._
+
+class RegisterBike {
+
+}
