28c16b3ea20447aae45c0a8debe030a7708e04de
Lukasz Lenart <lukasz.lenart@gmail.com>
2016-05-10 11:41:29 +0200

    Adds basic class

---
+++ src/main/scala/com/gruuf/snippet/BikeEvents.scala
@@ -0,0 +1,13 @@
+package com.gruuf.snippet
+
+import com.gruuf.model._
+import net.liftweb.http._
+import net.liftweb.sitemap.Loc._
+import net.liftweb.util.Helpers._
+import net.liftweb.sitemap._
+import net.liftweb.util._
+import org.joda.time.DateTime
+
+class BikeEvents {
+
+}
