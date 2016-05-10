0f38169aeb5525563782005967ad25a8d8f9641c
Lukasz Lenart <lukasz.lenart@gmail.com>
2016-05-10 10:50:36 +0200

    Adds basic imports

--- src/main/scala/com/gruuf/snippet/Home.scala
+++ src/main/scala/com/gruuf/snippet/Home.scala
@@ -1,7 +1,12 @@
 package com.gruuf.snippet
 
-import net.liftweb.sitemap.Menu
-import net.liftweb.util.PassThru
+import net.liftweb.http._
+import net.liftweb.sitemap._
+  import Menu._
+  import Loc._
+import net.liftweb.util._
+  import Helpers._
+
 
 object Home {
   val menu = Menu.i("Home") / "home"
